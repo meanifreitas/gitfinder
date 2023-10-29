269e88dac43777918c4f823d7bf9ee2987f18543
Meani Freitas <meani.sf@gmail.com>
2023-10-29 14:00:07 -0300

    Fetching and handling data

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -10,7 +10,20 @@ function App() {
   const [repos, setRepos] = useState(null);
 
   const handleGetData = async () => {
+    const userData = await fetch(`https://api.github.com/users/${user}`);
+    const newUser = await userData.json();
 
+   if (newUser.name) {
+    const { avatar_url, name, bio, login } = newUser;
+    setCurrentUser({ avatar_url, name, bio, login });
+
+    const reposData = await fetch(`https://api.github.com/users/${user}/repos`);
+    const newRepos = await reposData.json();
+
+    if (newRepos.length) {
+      setRepos(newRepos);
+    }
+   }
   }
   return (
     <div className='App'>
@@ -25,23 +38,28 @@ function App() {
               value={user}
               onChange={event => setUser(event.target.value)}>
             </input>
-            <button>Search</button>
+            <button onClick={handleGetData}>Search</button>
           </div>
-          <div className='profileContainer'>
-            <img src='https://avatars.githubusercontent.com/u/61467221?v=4' className='profile' alt='profile image'/>
-            <div>
-              <h3>Meani Freitas</h3>
-              <span>@meanifreitas</span>
-              <p>Description</p>
+          {currentUser?.name ? (
+          <>
+            <div className='profileContainer'>
+              <img src={currentUser.avatar_url} className='profile' alt='profile image'/>
+              <div>
+                <h3>{currentUser.name}</h3>
+                <span>@{currentUser.login}</span>
+                <p>{currentUser.bio}</p>
+              </div>
             </div>
-          </div>
-          <hr />
-          <div>
-            <h4 className='repositories'>Repositories</h4>
-            <ListItem title='rep1' description='description'></ListItem>
-            <ListItem title='rep1' description='description'></ListItem>
-            <ListItem title='rep1' description='description'></ListItem>
-          </div>
+            <hr />
+          </>) : null}
+          {repos?.length ? (<>
+            <div>
+              <h4 className='repositories'>Repositories</h4>
+              {repos.map(item => {
+                return <ListItem title={item.name} description={item.description}></ListItem>
+              })}
+            </div>
+          </>) : null}
         </div>
       </div>
     </div>
